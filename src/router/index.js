9db77c656f436a5c27b87ed53792580b64ab7dc6
fcjcbk <1097086201@qq.com>
2024-01-05 10:04:20 +0800

    Remove unused components

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,6 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Pods from '../components/Pods.vue'
 import Nodes from '../components/Nodes.vue'
 import CreatePod from '../components/CreatePod.vue'
-import Test from '../components/TestPod.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
